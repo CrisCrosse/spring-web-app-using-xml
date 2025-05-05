77a9e7f651dbabf6f2aae4174c3a34b8e944163f
Chris Rossell <chris.rossell@autotrader.co.uk>
2025-05-05 13:03:57 +0100

    Initial commit for springboot web MVC + thymeleaf initialised project

---
+++ src/main/java/com/example/spring_web_app_using_xml/SpringWebAppUsingXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_app_using_xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppUsingXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppUsingXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_web_app_using_xml/SpringWebAppUsingXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_app_using_xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppUsingXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
